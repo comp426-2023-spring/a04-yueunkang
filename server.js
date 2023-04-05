7a156ce65197a3f00bf9600b619af9b961e68f11
Yueun Kang <jkang003@email.unc.edu>
2023-04-05 11:29:12 -0400

    claening up the code

--- server.js
+++ server.js
@@ -4,25 +4,20 @@ import express from 'express'
 
 var argv = minimist(process.argv.slice(2));
 const port = argv.port || 5000;
-
 const app = express();
+
 app.use(express.json());
+
 app.use(express.urlencoded({extended: true}));
 
 app.get('/app', (req, res) => {
     res.status(200).send('200 OK').end();
 });
 
-/**
- * Random rps shot
- */
 app.get('/app/rps', (req, res) => {
     res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
 
-/**
- * Random rpsls shot
- */
 app.get('/app/rpsls', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
 })

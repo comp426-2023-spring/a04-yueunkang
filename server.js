4929548f795da43e51dc3c7c1022a396a68c19bb
Yueun Kang <jkang003@email.unc.edu>
2023-04-05 11:17:16 -0400

    server.js

--- server.js
+++ server.js
@@ -14,3 +14,26 @@ app.use(express.urlencoded({extended: true}));
 app.get('/app', (req, res) => {
     res.status(200).send('200 OK').end();
 });
+
+/**
+ * Random rps shot
+ */
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+
+/**
+ * Random rpsls shot
+ */
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+
+/**
+ * URLEncoded
+ * Note: This is a GET
+ * Example: http://localhost:5000/app/rps/play?shot=rock
+ */
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
+})

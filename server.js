a3cc1da47783f1dc5d5fafe0ac00a08aa7c8c102
Yueun Kang <jkang003@email.unc.edu>
2023-04-05 11:30:09 -0400

    claening up the code

--- server.js
+++ server.js
@@ -46,9 +46,6 @@ app.get('/app/rpsls/play/:shot', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
 })
 
-/**
- * Default (non-existent) endpoint
- */
 app.all('*', (req, res) => {
     res.status(404).send('404 NOT FOUND').end();
 })
